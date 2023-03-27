3026db7dcea4d8f4fb9e42769f2bcfbf559dd934
Vincent Kernivinen <115277048+kerzac@users.noreply.github.com>
2023-03-27 23:28:05 +0200

    make gallery filter functional

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,5 +1,9 @@
+/*
+    fetch works from api and generate them
+*/
+
 const works =  await fetch('http://localhost:5678/api/works')
-                    .then((response) => response.json())
+                    .then((response) => response.json());
 
 function generateWorks (works) {
     for (let i = 0; i < works.length; i++) {
@@ -18,3 +22,80 @@ function generateWorks (works) {
 }
 
 generateWorks(works);
+
+/*
+    create funtionnal filtering buttons
+*/
+
+const filterAll = document.querySelector('.filter-all');
+const filterObject = document.querySelector('.filter-object');
+const filterAppartment = document.querySelector('.filter-appartment');
+const filterHotel = document.querySelector('.filter-hotel');
+
+// display all works
+filterAll.addEventListener('click', () => {
+    const filterWork = works.filter((work) => work);
+
+    const filters = document.querySelectorAll('.filter');
+    filters.forEach((filter) => {
+        filter.style.backgroundColor = 'white';
+        filter.style.color = '#1D6154';
+    });
+    filterAll.style.backgroundColor = '#1D6154';
+    filterAll.style.color = 'white';
+
+    const galleryWork = document.querySelector('.gallery');
+    galleryWork.innerHTML='';
+    generateWorks(filterWork);
+});
+
+// display 'objects' works
+filterObject.addEventListener('click', () => {
+    const filterWork = works.filter((work) => work.categoryId == 1);
+
+    const filters = document.querySelectorAll('.filter');
+    filters.forEach((filter) => {
+        filter.style.backgroundColor = 'white';
+        filter.style.color = '#1D6154';
+    });
+    filterObject.style.backgroundColor = '#1D6154';
+    filterObject.style.color = 'white';
+
+    const galleryWork = document.querySelector('.gallery');
+    galleryWork.innerHTML='';
+    generateWorks(filterWork);
+});
+
+// display 'appartment' works
+filterAppartment.addEventListener('click', () => {
+    const filterWork = works.filter((work) => work.categoryId == 2);
+
+    const filters = document.querySelectorAll('.filter');
+    filters.forEach((filter) => {
+        filter.style.backgroundColor = 'white';
+        filter.style.color = '#1D6154';
+    });
+    filterAppartment.style.backgroundColor = '#1D6154';
+    filterAppartment.style.color = 'white';
+
+    const galleryWork = document.querySelector('.gallery');
+    galleryWork.innerHTML='';
+    generateWorks(filterWork);
+});
+
+// display 'hotel & restaurant' works
+filterHotel.addEventListener('click', () => {
+    const filterWork = works.filter((work) => work.categoryId == 3);
+
+    const filters = document.querySelectorAll('.filter');
+    filters.forEach((filter) => {
+        filter.style.backgroundColor = 'white';
+        filter.style.color = '#1D6154';
+    });
+    filterHotel.style.backgroundColor = '#1D6154';
+    filterHotel.style.color = 'white';
+
+    const galleryWork = document.querySelector('.gallery');
+    galleryWork.innerHTML='';
+    generateWorks(filterWork);
+});
