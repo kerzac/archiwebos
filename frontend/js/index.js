60c733a414c2e18a8e2034fb3c4162f29f5baca6
Vincent Kernivinen <115277048+kerzac@users.noreply.github.com>
2023-03-30 02:08:29 +0200

    add edition mode (non functional)

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -76,7 +76,9 @@ filterHotel.addEventListener('click', () => {
 
 const logIn = document.getElementById('logIn');
 logIn.addEventListener('click', () => localStorage.clear());
+const editions = document.querySelectorAll('.edition');
 
 if (window.localStorage.getItem('authentication')) {
     logIn.innerText = 'logout';
-}
+    editions.forEach((edition) => edition.classList.remove('hidden'));
+    }
